9cf8b06eb1e5df2fa2aeae6dbcb0d8822000d390
sadfool1 <takw14@gmail.com>
2021-09-27 14:45:22 +0800

    rendering new tiles

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom";
 
-import './index.css';
-import App from './App';
+import "./index.css";
+import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
+// takes 2 args 2nd one is default JS dom object
+
+//public file contains index.html. With
