42b8db1e7e277fcd0552377664998e9f7ea37fc1
sadfool1 <takw14@gmail.com>
2021-09-27 19:30:37 +0800

    added forms

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,21 @@
+import React, { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = props => {
+  const [title, setTitle] = useState(props.title);
+  // useState is special variable and manage React in some memory
+
+  const clickerHandler = () => setTitle("Updated");
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2> {props.title} </h2>
+        <h2> {title} </h2>
         <div className="expense-item__price"> ${props.amount} </div>
       </div>
+      <button onClick={clickerHandler}>Change Title</button>
     </Card>
   );
 };
