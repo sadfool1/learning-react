5f12914a7e592ecdf3bb133db0b9582238ca9042
sadfool1 <takw14@gmail.com>
2021-11-21 17:09:55 +0800

    Added new components forM

--- CartApp/01-starting-project/src/components/Layout/Header.js
+++ CartApp/01-starting-project/src/components/Layout/Header.js
@@ -1,18 +1,17 @@
 import React, { Fragment } from "react";
 import classes from "./Header.module.css";
+import mealsImage from "../../assets/meals.jpg";
+import HeaderCartButton from "./HeaderCartButton";
 
-const mealsImage =
-  "https://github.com/academind/react-complete-guide-code/blob/11-practice-food-order-app/extra-files/meals.jpg";
-const Header = (props) => {
+const Header = props => {
   return (
     <Fragment>
       <header className={classes.header}>
         <h1> React Meals </h1>
-        <button> Cart </button>
+        <HeaderCartButton />
       </header>
       <div className={classes["main-image"]}>
-        {" "}
-        /* cannot use dot notation bc of the dash */{" "}
+        /* cannot use dot notation bc of the dash */
         <img src={mealsImage} alt="A table full of delicious food!" />
       </div>
     </Fragment>
