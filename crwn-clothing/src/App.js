5088a1244b29a33bc7b17a6a1a3e896c96f68a83
James Morillo <jamesmorillo@ohmyhome.com>
2022-03-08 00:00:57 +0800

    added router options

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import './App.css';
 import HomePage from "./pages/homepage/homepage.component"
+import { Switch, Route } from 'react-router-dom';
+
+
+const HatsPage = () => {
+
+  <div className="">
+    <h1 className=""> HATSPAGE </h1>
+  </div>
+}
 
 function App() {
-  return ( <HomePage> </HomePage>
+  return (
+    <div className="">
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
+
+     </div>
+    // <HomePage> </HomePage>
   );
 }
 
